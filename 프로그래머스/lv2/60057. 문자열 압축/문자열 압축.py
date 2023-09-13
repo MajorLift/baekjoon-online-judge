216aac3466eea56896f319a7a5d802674586bf1a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-12 22:40:35 -0700

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,22 +1,24 @@
 def solution(s):
-    s_len = len(s)
-    min_len = s_len
+    slen = len(s)
+    minlen = slen
     
-    for slice_ in range(1, s_len // 2 + 1):
-        pattern_cnt, last_pattern = [], ''
-        for j in range(1, s_len // slice_ + 1):
+    for slice_ in range(1, slen // 2 + 1):
+        patternCounts = []
+        last_pattern = ""
+        for j in range(1, slen // slice_ + 1):
             pattern = s[slice_ * (j-1) : slice_ * j]
             if last_pattern == pattern:
-                pattern_cnt[-1] += 1
+                patternCounts[-1] += 1
             else:
-                pattern_cnt.append(1)
+                patternCounts.append(1)
             last_pattern = pattern
 
-        curr_len = s_len
-        for cnt in [x for x in pattern_cnt if x > 1]:
-            curr_len -= slice_ * (cnt - 1)
-            curr_len += len(str(cnt))
+        curlen = slen
+        patternCounts = [x for x in patternCounts if x > 1]
+        for count in patternCounts:
+            curlen -= slice_ * (count - 1)
+            curlen += len(str(count))
+        if curlen < minlen:
+            minlen = curlen
         
-        min_len = min(min_len, curr_len)
-        
-    return min_len
+    return minlen
